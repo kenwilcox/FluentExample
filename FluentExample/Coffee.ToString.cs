8b3d573bbd6c250eadc4747094bc46520cd7d418
Ken Wilcox <wilcoxk@gmail.com>
2015-07-27 17:01:23 -0600

    Ugh - tabs

--- FluentExample/Coffee.ToString.cs
+++ FluentExample/Coffee.ToString.cs
@@ -11,10 +11,10 @@ namespace FluentExample
             var propInfo = GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
             foreach (var prop in propInfo.Where(prop => !prop.Name.StartsWith("Raw")))
             {
-	            if (ret.Length > 0)
-		            ret += string.Format(" - {0}: {1}", prop.Name, prop.GetValue(this));
-	            else
-		            ret += string.Format("{0}: {1}", prop.Name, prop.GetValue(this));
+                if (ret.Length > 0)
+                    ret += string.Format(" - {0}: {1}", prop.Name, prop.GetValue(this));
+                else
+                    ret += string.Format("{0}: {1}", prop.Name, prop.GetValue(this));
             }
             return ret;
         }
